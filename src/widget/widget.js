2af1d7ad7e12324f6c74d5fbaadf34484a86b557
pedroslevv <pedro.lopez@philips.edu.ar>
2020-02-29 19:11:53 -0300

    a

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -78,35 +78,9 @@ export default class Widget extends Component {
         let stateData = {
             pristine: false,
             isChatOpen: !this.state.isChatOpen,
+            wasChatOpened: false,
         }
-        if(!this.state.isChatOpen && !this.wasChatOpened()){
-            this.setCookie();
-            stateData.wasChatOpened = true;
-        }
-        this.setState(stateData);
     }
 
-    setCookie = () => {
-        let date = new Date();
-        let expirationTime = parseInt(this.props.conf.cookieExpiration);
-        date.setTime(date.getTime()+(expirationTime*24*60*60*1000));
-        let expires = "; expires="+date.toGMTString();
-        document.cookie = "chatwasopened=1"+expires+"; path=/";
-    }
-
-    getCookie = () => {
-        var nameEQ = "chatwasopened=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return false;
-    }
-
-    wasChatOpened = () => {
-        return (this.getCookie() === false) ? false : true;
-    }
 
 }
