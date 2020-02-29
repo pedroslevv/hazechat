33f33ef20a3141e854ce1d655797c6cf85e5d1ef
pedroslevv <pedro.lopez@philips.edu.ar>
2020-02-29 18:51:25 -0300

    Update widget-index.js

--- src/widget/widget-index.js
+++ src/widget/widget-index.js
@@ -10,12 +10,12 @@ if (window.attachEvent) {
 
 function injectChat() {
     if (!window.intergramId) {
-        console.error('Please set window.intergramId (see example at github.com/idoco/intergram)');
+        console.error('Please set window (see example at github.com/pedroslevv/hazechat)');
     } else {
         let root = document.createElement('div');
         root.id = 'intergramRoot';
         document.getElementsByTagName('body')[0].appendChild(root);
-        const server = window.intergramServer || 'https://www.intergram.xyz';
+        const server = window.intergramServer || 'https://www.haze-chatwidgeter.herokuapp.com';
         const iFrameSrc = server + '/chat.html';
         const host = window.location.host || 'unknown-host';
         const conf = { ...defaultConfiguration, ...window.intergramCustomizations };
