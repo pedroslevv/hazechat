9a89968efba7bf615ce70e4f7593e59a0bc95307
pedroslevv <pedro.lopez@philips.edu.ar>
2020-03-26 15:58:22 -0300

    Update default-configuration.js

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -7,18 +7,7 @@ export const defaultConfiguration = {
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
     introMessage: 'Hello! How can we help you?',
     autoResponse: '¡Hola!',
-    autoNoResponse: document.getElementById('btn').onclick = function() {
-            var val = 'Horas Enero',
-                src = 'http://3.16.29.118:5000/processText?userId=7878787878&text=' + val +'.png',
-                img = document.createElement('img');
-
-            img.src = src;
-            document.body.appendChild(img);
-        }
-    
-    
-    
-    ,
+    autoNoResponse: 'http://3.16.29.118:5000/processText?userId=7878787878&text=Horas%20enero',
     placeholderText: 'Envia un mensaje...',
     displayMessageTime: true,
     mainColor: '#4e89e8',
